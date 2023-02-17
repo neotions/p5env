25c1d95384398a8a210082880c9d5829a120328c
neotions <johnmsthilaire@gmail.com>
2023-02-16 19:43:01 -0500

    updated html head

--- sketches/hexagons.js
+++ sketches/hexagons.js
@@ -1,12 +1,12 @@
 function isClose(pos1,pos2,gap,dot_size) {
 
-    
-    
-
         x1 = pos1[0];
         y1 = pos1[1];
         x2 = pos2[0];
         y2 = pos2[1];
+
+        r1 = pos1[2];
+        r2 = pos2[2];
 
         d = sqrt( sq(x2-x1) + sq(y2-y1) )
         
@@ -14,14 +14,11 @@ function isClose(pos1,pos2,gap,dot_size) {
             //console.log(d)
         }
 
-        if (d < dot_size + gap) {
+        if (d < r1 + r2 + gap) {
             return true
         }
         return false
     }
-
-
-
 
 function isOff(pos) {
     x = pos[0];
@@ -32,4 +29,16 @@ function isOff(pos) {
     else {
         return false;
     }
+}
+
+function setup(){
+    canvas = createCanvas(800,800);
+    noLoop();
+}
+
+function draw() {
+    
+    
+
+
 }
