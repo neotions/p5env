464ff9fc67b6b70ee739766041cd20e1a6f57a5e
neotions <johnmsthilaire@gmail.com>
2023-02-17 01:30:01 -0500

    updating

--- sketches/hexagons.js
+++ sketches/hexagons.js
@@ -1,19 +1,30 @@
-function isClose(pos1,pos2,gap,dot_size) {
+function hexagon(sx,sy,size) {
+    let side = (size/2) * 1.1547005;
+      let angle = TWO_PI / 6;
+    let x = sx + side * cos(0);
+    let y = sy + side * sin(0);
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      x = sx + side * cos(a);
+      y = sy + side * sin(a);
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+  }
+  
+
+function isClose(pos1,pos2,gap) {
 
         x1 = pos1[0];
         y1 = pos1[1];
         x2 = pos2[0];
         y2 = pos2[1];
 
-        r1 = pos1[2];
-        r2 = pos2[2];
+        r1 = (pos1[2] / 2) / cos(TWO_PI/12);
+        r2 = (pos2[2] / 2) / cos(TWO_PI/12);
 
         d = sqrt( sq(x2-x1) + sq(y2-y1) )
         
-        if (i == 0 ) {
-            //console.log(d)
-        }
-
         if (d < r1 + r2 + gap) {
             return true
         }
@@ -33,12 +44,48 @@ function isOff(pos) {
 
 function setup(){
     canvas = createCanvas(800,800);
+
+    objs = [[random(width),random(height),random(0,height/2)]];
+
     noLoop();
+
+    background(255)
 }
 
 function draw() {
     
-    
+    for (let i = 0; i < 9999; i++) {
+
+        good = true;
+
+        pos1 = [random(width),random(height),random(20,height/2)]
+
+        for (let j = 0; j < objs.length; j++) {
+             
+            console.log(objs.length);
+
+            if ( isClose(pos1, objs[j],0) || isOff(pos1) ) {
+                good = false;
+            }
+
+
+        }
+
+
+        if ( good ) {
+            fill(255);
+            hexagon(pos1[0],pos1[1],pos1[2]);
+            
+            noFill()
+            circle(pos1[0],pos1[1],pos1[2] / cos(TWO_PI/12));
+
+            objs.push(pos1);
+        }
+        else {
+            console.log("nope");
+        }
+
+    }
 
 
 }
